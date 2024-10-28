e7a482f3c98f369f002c8a3c72687377511dfa58
Krzysztof Wąsik <krzysztof.wasik2002@gmail.com>
2024-10-28 10:58:39 +0100

    feat: Add geolocation permissions and map initialization
    - Added "deviceready" event listener to request geolocation permission
    - Implemented geolocation integration and map centering based on user's location
    - Updated Leaflet map setup with basic view, tile layer, and initial marker

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,13 @@ document.addEventListener('deviceready', function () {
 });
 
 function initApp() {
+    console.log("Initializing the app...");
+
+    if (typeof L === 'undefined') {
+        console.error("Leaflet library (L) is not loaded!");
+        return;
+    }
+
     var url = "http://10.0.2.2:3000/api/routes"; 
     cordova.plugin.http.get(url, {}, {}, function (response) {
         try {
@@ -30,18 +37,24 @@ function initApp() {
         console.error(error);
     });
 
+    console.log("Setting up the map...");
+
     var map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
 
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
     function onSuccess(position) {
         console.log('Latitude: ' + position.coords.latitude + '\n' +
                     'Longitude: ' + position.coords.longitude + '\n');
-        map.setView([position.coords.latitude, position.coords.longitude], 13); 
+        map.setView([position.coords.latitude, position.coords.longitude], 13);
     }
 
     function onError(error) {
@@ -49,34 +62,41 @@ function initApp() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-});
 
 function displayRoutes(routes) {
     var routeList = document.getElementById("route-list");
-    routes.forEach(route => {
-        let li = document.createElement("li");
-        li.innerHTML = route.name + ": " + route.description;
-        
-        let selectButton = document.createElement("button");
-        selectButton.innerText = "Select Route";
-        selectButton.onclick = function() {
-            selectRoute(route.id);
-        };
-        
-        li.appendChild(selectButton);
-        routeList.appendChild(li);
-    });
+    
+    if (routeList) {
+        console.log("Route list element found. Displaying routes...");
+
+        routes.forEach(route => {
+            let li = document.createElement("li");
+            li.innerHTML = route.name + ": " + route.description;
+
+            let selectButton = document.createElement("button");
+            selectButton.innerText = "Select Route";
+            selectButton.onclick = function() {
+                selectRoute(route.id);
+            };
+
+            li.appendChild(selectButton);
+            routeList.appendChild(li);
+        });
+    } else {
+        console.error("Route list element not found!");
+    }
 }
 
 function selectRoute(routeId) {
     var url = `http://10.0.2.2:3000/api/routes/${routeId}/waypoints`; 
-    
+
+    console.log(`Fetching waypoints for route ID: ${routeId}...`);
+
     cordova.plugin.http.get(url, {}, {}, function (response) {
         let waypoints = JSON.parse(response.data);
         displayWaypoints(waypoints);
     }, function(error) {
-        console.error(error);
+        console.error("Error fetching waypoints: ", error);
     });
 }
 
@@ -85,4 +105,3 @@ function displayWaypoints(waypoints) {
         console.log(`Waypoint: ${waypoint.name}, Coordinates: (${waypoint.lat}, ${waypoint.lng})`);
     });
 }
-
